5e94ce863c68240f0ff4e24fa913bfc0052b0b7d
Ruchik <ruchik.panwal@gmail.com>
2024-06-07 16:12:40 +0530

    grid with custom resolution created

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+
+gridCreator(16);
+
+function gridCreator(num) {
+
+    for (let i = 0; i < num; i++) {
+
+        const mainGrid = document.querySelector("#mainGrid");
+
+        const row = document.createElement("div");
+        row.classList.add("row");
+        mainGrid.appendChild(row);
+
+        for (let j = 0; j < num; j++) {
+
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+        }
+    }
+}
