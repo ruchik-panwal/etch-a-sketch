e9b2dc4d287589e941d1da042c543dc97dadf93c
Ruchik <ruchik.panwal@gmail.com>
2024-06-10 00:40:26 +0530

    I am struggling too write commit mesages T_T

--- javascript.js
+++ javascript.js
@@ -1,21 +1,46 @@
-//@ts-check
+
 const mainGrid = document.querySelector("#mainGrid");
 const slider = document.getElementById("myRange");
 const gridInfo = document.querySelector("#gridInfo")
 
 gridCreator(slider.value);
-gridInfo.textContent = (slider.value) + " X " + (slider.value) ;
+gridInfo.textContent = (slider.value) + " X " + (slider.value);
+
+// Changing resolution by using buttons
+const preset = document.querySelectorAll(".presets");
+
+preset.forEach((preset) => {
+
+    preset.addEventListener('click', () => {
+
+        if (preset.id == "8")
+            slider.value = 8;
+
+        else if (preset.id == "16")
+            slider.value = 16;
+
+        else if (preset.id == "32")
+            slider.value = 32;
+
+        else
+            slider.value = 64;
+
+        gridCreator(slider.value);
+        gridInfo.textContent = (slider.value) + " X " + (slider.value);
+    })
+})
 
 slider.oninput = function () {
-    gridCreator(this.value)
-    gridInfo.textContent = (this.value) + " X " + (this.value) ;
+    gridCreator(this.value);
+    gridInfo.textContent = (this.value) + " X " + (this.value);
 }
+
+
 
 
 function gridCreator(num) {
 
-   if(mainGrid.hasChildNodes)
-    {
+    if (mainGrid.hasChildNodes) {
         mainGrid.textContent = '';
     }
 
@@ -33,4 +58,43 @@ function gridCreator(num) {
 
         }
     }
+
+    //GRID BORDER TOGGLE
+    const element = document.querySelectorAll(".column");
+    const gborder = document.querySelector(".gridToggle");
+
+    gborder.addEventListener('click', () => {
+
+        switch (gborder.value) {
+
+            case "1":
+                {
+                    element.forEach((element) => {
+
+                        element.setAttribute("style", "border-style: none;");
+
+                    });
+                    gborder.value = "0";
+                    break;
+                }
+
+            case "0": {
+                element.forEach((element) => {
+                    element.setAttribute("style", "border-style: solid;");
+
+                });
+                gborder.value = "1";
+                break;
+            }
+        }
+    });
 }
+
+
+
+
+//Changing the border style 
+
+
+
+
