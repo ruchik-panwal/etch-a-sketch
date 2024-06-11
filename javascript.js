a10fb1842d0815467c78644a39a4b9c397ec01d6
Ruchik <ruchik.panwal@gmail.com>
2024-06-11 14:32:24 +0530

    Grid tab Completed : completed the dynamic preset toggle

--- javascript.js
+++ javascript.js
@@ -4,21 +4,11 @@ const mainGrid = document.querySelector("#mainGrid");
 const slider = document.getElementById("myRange");
 const gridInfo = document.querySelector("#gridInfo");
 
-//For showing the grid or not
+//For the default toggle values
 const gborder = document.querySelector(".gridToggle");
+gborder.setAttribute("style", "border-style: solid; border-color: white;");
+
 let element;
-
-
-//Toggle Button for showing grid
-gborder.addEventListener('click', () => {
-    if (gborder.value == "1") {
-        gborder.value = "0";
-    }
-    else {
-        gborder.value = "1";
-    }
-    gridCreator(slider.value);
-});
 
 //Updating the slider value to everything
 gridCreator(slider.value);
@@ -28,10 +18,11 @@ gridInfo.textContent = (slider.value) + " X " + (slider.value);
 const preset = document.querySelectorAll(".presets");
 preset.forEach((preset) => {
 
-    preset.addEventListener('click', () => {
+    preset.addEventListener('mousedown', () => {
 
         if (preset.id == "8")
             slider.value = 8;
+
 
         else if (preset.id == "16")
             slider.value = 16;
@@ -90,20 +81,50 @@ function gridCreator(num) {
             element.setAttribute("style", "border-style: solid;");
         });
     }
+
+    //PRESET TOGGLE
+
+    const tpreset = document.querySelectorAll(".presets");
+    tpreset.forEach((tpreset) => {
+        if (num == 8 || num == 16 || num == 32 || num == 64) {
+            if (tpreset.id == "8" && num == 8)
+                tpreset.setAttribute("style", "border-style: solid; border-color: white;");
+
+            if (tpreset.id == "16" && num == 16)
+                tpreset.setAttribute("style", "border-style: solid; border-color: white;");
+
+            if (tpreset.id == "32" && num == 32)
+                tpreset.setAttribute("style", "border-style: solid; border-color: white;");
+
+            if (tpreset.id == "64" && num == 64)
+                tpreset.setAttribute("style", "border-style: solid; border-color: white;");
+        }
+        else
+            tpreset.setAttribute("style", "border-style: none; ");
+    });
+
 }
 
 
-
 const button = document.querySelectorAll("button");
+
 button.forEach((button) => {
 
     button.addEventListener('mousedown', () => {
 
-        if (button.id == "toggle")
-            button.setAttribute("style", "border-style: solid; border-color: white;");
 
+        //For the grid to toggle show
+        if (button.id == "gtoggle") {
+            if (gborder.value == "1") {
+                gborder.value = "0";
+                gborder.setAttribute("style", "border-style: solid; border-color: white;");
+            }
+            else {
+                gborder.value = "1";
+                gborder.setAttribute("style", "border-style: none;");
+            }
+            gridCreator(slider.value);
+        }
 
     });
-
 });
-
